196faeab925f610a3765efd9aeb78c8a55616c91
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-09-02 11:37:11 +0300

    Added simple HTML and simple JS functions

---
+++ app.js
@@ -0,0 +1,28 @@
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
+
+function operate(operator, firstNum, secondNum) {
+  switch (operator) {
+    case "+":
+      return add(firstNum, secondNum);
+    case "-":
+      return subtract(firstNum, secondNum);
+    case "*":
+      return multiply(firstNum, secondNum);
+    case "/":
+      return divide(firstNum, secondNum);
+  }
+}
